d6adfd9a69682a883ed136c435f8fff3128b2de1
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-10-06 23:38:57 +0300

    Add connectDB

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const app = require('./app');
+const connectDB = require('./config/db');
+
+connectDB();
+
+const server = http.createServer(app);
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log('Server started at port ', port);
+});
