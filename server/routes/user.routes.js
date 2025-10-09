9d540e39f7a289da8b559934d0c3fdb7e0c15e6f
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-10-09 22:19:53 +0300

    Add getAllUsers

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { registerSchema, loginSchema } = require('../validators/user.validator');
-const { registerUser, loginUser, getAccount } = require('../controllers/user.controller');
+const { registerUser, loginUser, getAccount, getAllUsers } = require('../controllers/user.controller');
 const { validate } = require('../middlewares/validate.mw');
-const { auth } = require('../middlewares/auth.mw');
+const { auth, isAdmin } = require('../middlewares/auth.mw');
 
 const router = express.Router();
 
 router.post('/register', validate(registerSchema), registerUser);
 router.post('/login', validate(loginSchema), loginUser);
 router.get('/account', auth, getAccount);
+router.get('/', auth, isAdmin, getAllUsers);
 
 module.exports = router;
