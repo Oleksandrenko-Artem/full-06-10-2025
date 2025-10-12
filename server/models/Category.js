e956ccd262892cec59ee3893b7b29c11669e1661
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-10-12 21:34:07 +0300

    Add Product model

--- server/models/Category.js
+++ server/models/Category.js
@@ -4,6 +4,15 @@ const categorySchema = new mongoose.Schema({
     name: { type: String, minLength: 3, maxLength: 64, required: true, unique: true },
 });
 
+categorySchema.virtual('products', {
+    ref: 'Product',
+    localField: '_id',
+    foreignField: 'category'
+});
+
+categorySchema.set('toObject', { virtual: true });
+categorySchema.set('toJSON', { virtual: true });
+
 const Category = mongoose.model('Category', categorySchema);
 
 module.exports = Category;
---
+++ server/models/Product.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const CONSTANTS = require('../constants');
+
+const productSchema = new mongoose.Schema({
+    title: { type: String, minLength: 3, maxLength: 255, required: true },
+    description: { type: String },
+    price: { type: Number, min: 0.01, max: 1000000000, required: true },
+    stockQty: { type: Number, min: 0, max: 1000, default: 1 },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+    isSale: { type: Boolean, default: false },
+    images: { type: [String], validate: [(arr) => arr.length <= CONSTANTS.MAX_LIMIT_IMG, 'Limit images = ' + CONSTANTS.MAX_LIMIT_IMG] },
+}, { timestamps: true });
+
+productSchema.index({ title: 1, category: 1 }, { unique: true });
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
